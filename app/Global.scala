0977842345f0c72b182a315356ac2c5471b5db8e
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2013-05-20 19:01:00 +0200

    fixed scaldi integration problems

--- app/Global.scala
+++ app/Global.scala
@@ -1,13 +1,27 @@
 import anorm.Id
+import controllers.auth.AuthModule
+import controllers.ConfigModule
 import models.User
 import play.api._
+import scaldi.ClassIdentifier
 
 object Global extends GlobalSettings {
+
+  private lazy val applicationModule = new AuthModule :: new ConfigModule
 
   override def onStart(app: Application) {
     InitialData.insert()
     Logger.info("Application has started")
   }
+
+
+  override def getControllerInstance[A](controllerClass: Class[A]) =
+    applicationModule.getBinding(List(ClassIdentifier(controllerClass))) match {
+      case Some(binding) => binding.get map (_.asInstanceOf[A]) getOrElse
+        (throw new IllegalStateException(s"Controller for class $controllerClass is explicitly un-bound!"))
+      case None =>
+        throw new IllegalStateException(s"Controller for class $controllerClass not found!")
+    }
 
   /**
    * Initial set of data to be imported
